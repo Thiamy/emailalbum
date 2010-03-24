4dd7c7377e52d370aed641a6824af7081e70a9d7
kevin.gaudin <kevin.gaudin@7dd601f1-8d53-0410-be79-7bf68d38fa1c>
2010-03-24 23:12:17 +0000

    Handling oc cache clearing from preference screens

--- EmailAlbumAndroid/trunk/src/com/kg/emailalbum/mobile/EmailAlbumPreferences.java
+++ EmailAlbumAndroid/trunk/src/com/kg/emailalbum/mobile/EmailAlbumPreferences.java
@@ -21,10 +21,13 @@ package com.kg.emailalbum.mobile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
+import android.widget.Toast;
 
 import com.kg.emailalbum.mobile.R;
+import com.kg.emailalbum.mobile.util.CacheManager;
 
 /**
  * Standard preferences activity.
@@ -33,20 +36,41 @@ public class EmailAlbumPreferences extends PreferenceActivity {
     public static final String EXTRA_SCREEN = "SCREEN";
     public static final String SCREEN_VIEWER = "viewerscreen";
     public static final String SCREEN_CREATOR = "creatorscreen";
-    
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.preference.PreferenceActivity#onCreate(android.os.Bundle)
-	 */
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.xml.viewer_preferences);
-		String screen = getIntent().getStringExtra(EXTRA_SCREEN);
-        if(screen != null) {
-            setPreferenceScreen((PreferenceScreen)findPreference(screen));
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.preference.PreferenceActivity#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.viewer_preferences);
+        String screen = getIntent().getStringExtra(EXTRA_SCREEN);
+        if (screen != null) {
+            setPreferenceScreen((PreferenceScreen) findPreference(screen));
         }
-	}
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @seeandroid.preference.PreferenceActivity#onPreferenceTreeClick(android.
+     * preference.PreferenceScreen, android.preference.Preference)
+     */
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            Preference preference) {
+        if (preference.getKey().equals("clearcache")) {
+            CacheManager cm = new CacheManager(getApplicationContext());
+            int nbDeleted = CacheManager.deleteDirectory(cm.getCacheDir());
+            
+            Toast.makeText(
+                    getApplicationContext(),
+                    String.format(getString(R.string.number_files_deleted),
+                            nbDeleted), Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
 
 }
