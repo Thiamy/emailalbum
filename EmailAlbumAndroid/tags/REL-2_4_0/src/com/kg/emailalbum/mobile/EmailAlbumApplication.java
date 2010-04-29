e9b199c5b73f7f737ac272716cf27913f95ed8d0
kevin.gaudin <kevin.gaudin@7dd601f1-8d53-0410-be79-7bf68d38fa1c>
2010-04-29 14:20:38 +0000

    - Albums can be exported as simple zip archives - Fixed critical issue when opening album from gmail

---
+++ EmailAlbumAndroid/tags/REL-2_4_0/src/com/kg/emailalbum/mobile/EmailAlbumApplication.java
@@ -0,0 +1,12 @@
+package com.kg.emailalbum.mobile;
+
+import org.acra.CrashReportingApplication;
+
+public class EmailAlbumApplication extends CrashReportingApplication {
+
+    @Override
+    public String getFormId() {
+        return "dGpqbndMSW96S3FDZ1h3REVoaGdmTWc6MQ";
+    }
+
+}
