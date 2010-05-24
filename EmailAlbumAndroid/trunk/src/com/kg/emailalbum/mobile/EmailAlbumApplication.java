396ba2700fed77c8736fb9c377ac83adf0122af7
kevin.gaudin <kevin.gaudin@7dd601f1-8d53-0410-be79-7bf68d38fa1c>
2010-05-24 00:08:37 +0000

    ACRA - use Toast mode

--- EmailAlbumAndroid/trunk/src/com/kg/emailalbum/mobile/EmailAlbumApplication.java
+++ EmailAlbumAndroid/trunk/src/com/kg/emailalbum/mobile/EmailAlbumApplication.java
@@ -14,15 +14,7 @@ public class EmailAlbumApplication extends CrashReportingApplication {
     @Override
     public Bundle getCrashResources() {
         Bundle result = new Bundle();
-        result.putInt(RES_NOTIF_TICKER_TEXT, R.string.crash_notif_ticker_text);
-        result.putInt(RES_NOTIF_TITLE, R.string.crash_notif_title);
-        result.putInt(RES_NOTIF_TEXT, R.string.crash_notif_text);
-        result.putInt(RES_NOTIF_ICON, android.R.drawable.stat_notify_error); // optional. default is a warning sign
-        result.putInt(RES_DIALOG_TEXT, R.string.crash_dialog_text);
-        result.putInt(RES_DIALOG_ICON, android.R.drawable.ic_dialog_info); //optional. default is a warning sign
-        result.putInt(RES_DIALOG_TITLE, R.string.crash_dialog_title); // optional. default is your application name 
-        result.putInt(RES_DIALOG_COMMENT_PROMPT, R.string.crash_dialog_comment_prompt); // optional. when defined, adds a user text field input with this text resource as a label
-        result.putInt(RES_DIALOG_OK_TOAST, R.string.crash_dialog_ok_toast); // optional. Displays a Toast when the user accpets to send a report ("Thank you !" for example)
+        result.putInt(RES_TOAST_TEXT, R.string.crash_toast_text);
         return result;
     }
 
