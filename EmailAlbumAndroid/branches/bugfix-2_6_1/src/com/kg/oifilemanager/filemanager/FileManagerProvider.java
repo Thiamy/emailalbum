97b6e8903abdb496c5733ced4d30a8222165900b
kevin.gaudin <kevin.gaudin@7dd601f1-8d53-0410-be79-7bf68d38fa1c>
2010-08-29 15:53:24 +0000

    Return null when file not found instead of NPE...

--- EmailAlbumAndroid/branches/bugfix-2_6_1/src/com/kg/oifilemanager/filemanager/FileManagerProvider.java
+++ EmailAlbumAndroid/branches/bugfix-2_6_1/src/com/kg/oifilemanager/filemanager/FileManagerProvider.java
@@ -23,102 +23,110 @@ import com.kg.oifilemanager.filemanager.util.MimeTypes;
 
 public class FileManagerProvider extends ContentProvider {
 
-	private static final String TAG = "FileManagerProvider";
-	public static final String AUTHORITY = "com.kg.emailalbum.share";
-	public static final String CONTENT_URI_STRING = ContentResolver.SCHEME_CONTENT + "://" + AUTHORITY;
-	public static final Uri CONTENT_URI = Uri.parse(CONTENT_URI_STRING);
-	private MimeTypes mMimeTypes;
+    private static final String LOG_TAG = "FileManagerProvider";
+    public static final String AUTHORITY = "com.kg.emailalbum.share";
+    public static final String CONTENT_URI_STRING = ContentResolver.SCHEME_CONTENT
+            + "://" + AUTHORITY;
+    public static final Uri CONTENT_URI = Uri.parse(CONTENT_URI_STRING);
+    private MimeTypes mMimeTypes;
 
-	@Override
-	public boolean onCreate() {
-		getMimeTypes();
-		return true;
-	}
+    @Override
+    public boolean onCreate() {
+        getMimeTypes();
+        return true;
+    }
 
-	/**
+    /**
 	 * 
 	 */
-	private void getMimeTypes() {
-		MimeTypeParser mtp = new MimeTypeParser();
+    private void getMimeTypes() {
+        MimeTypeParser mtp = new MimeTypeParser();
 
-		XmlResourceParser in = getContext().getResources().getXml(
-				R.xml.mimetypes);
+        XmlResourceParser in = getContext().getResources().getXml(
+                R.xml.mimetypes);
 
-		try {
-			mMimeTypes = mtp.fromXmlResource(in);
-		} catch (XmlPullParserException e) {
-			Log
-					.e(
-							TAG,
-							"PreselectedChannelsActivity: XmlPullParserException",
-							e);
-			throw new RuntimeException(
-					"PreselectedChannelsActivity: XmlPullParserException");
-		} catch (IOException e) {
-			Log.e(TAG, "PreselectedChannelsActivity: IOException", e);
-			throw new RuntimeException(
-					"PreselectedChannelsActivity: IOException");
-		}
-	}
+        try {
+            mMimeTypes = mtp.fromXmlResource(in);
+        } catch (XmlPullParserException e) {
+            Log.e(LOG_TAG, "PreselectedChannelsActivity: XmlPullParserException", e);
+            throw new RuntimeException(
+                    "PreselectedChannelsActivity: XmlPullParserException");
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "PreselectedChannelsActivity: IOException", e);
+            throw new RuntimeException(
+                    "PreselectedChannelsActivity: IOException");
+        }
+    }
 
-	@Override
-	public int delete(Uri uri, String s, String[] as) {
-		// not supported
-		return 0;
-	}
+    @Override
+    public int delete(Uri uri, String s, String[] as) {
+        // not supported
+        return 0;
+    }
 
-	@Override
-	public String getType(Uri uri) {
-		// return file extension (uri.lastIndexOf("."))
-		return mMimeTypes.getMimeType(uri.toString());
-	}
+    @Override
+    public String getType(Uri uri) {
+        // return file extension (uri.lastIndexOf("."))
+        return mMimeTypes.getMimeType(uri.toString());
+    }
 
-	@Override
-	public Uri insert(Uri uri, ContentValues contentvalues) {
-		// not supported
-		return null;
-	}
+    @Override
+    public Uri insert(Uri uri, ContentValues contentvalues) {
+        // not supported
+        return null;
+    }
 
-	@Override
-	public Cursor query(Uri uri, String[] as, String s, String[] as1, String s1) {
-		if (uri.toString().startsWith(
-				CONTENT_URI_STRING)) {
-			MatrixCursor c = new MatrixCursor(new String[] { Images.Media.DATA,
-					Images.Media.MIME_TYPE });
-			String data = uri.toString().substring(CONTENT_URI_STRING.length());
-			String mimeType = mMimeTypes.getMimeType(data);
-			c.addRow(new String[] { data, mimeType });
-			return c;
-		} else {
-			throw new RuntimeException("Unsupported uri");
-		}
-	}
-	
-	@Override
-	public ParcelFileDescriptor openFile(Uri uri, String mode)
-			throws FileNotFoundException {
-		if (uri.toString().startsWith(
-				CONTENT_URI_STRING)) {
-			int m = ParcelFileDescriptor.MODE_READ_ONLY;
-			if (mode.equalsIgnoreCase("rw"))
-				m = ParcelFileDescriptor.MODE_READ_WRITE;
-			
-			File f = new File(uri.toString().substring(CONTENT_URI_STRING.length()));
-			ParcelFileDescriptor pfd = ParcelFileDescriptor.open(f, m);
-			return pfd;
-		} else {
-			throw new RuntimeException("Unsupported uri");
-		}
-	}
+    @Override
+    public Cursor query(Uri uri, String[] as, String s, String[] as1, String s1) {
+        if (uri.toString().startsWith(CONTENT_URI_STRING)) {
+            MatrixCursor c = new MatrixCursor(new String[] { Images.Media.DATA,
+                    Images.Media.MIME_TYPE });
+            String data = uri.toString().substring(CONTENT_URI_STRING.length());
+            String mimeType = mMimeTypes.getMimeType(data);
+            c.addRow(new String[] { data, mimeType });
+            return c;
+        } else {
+            throw new RuntimeException("Unsupported uri");
+        }
+    }
 
-	@Override
-	public int update(Uri uri, ContentValues contentvalues, String s,
-			String[] as) {
-		// not supported
-		return 0;
-	}
+    @Override
+    public ParcelFileDescriptor openFile(Uri uri, String mode)
+            throws FileNotFoundException {
+        if (uri.toString().startsWith(CONTENT_URI_STRING)) {
+            int m = ParcelFileDescriptor.MODE_READ_ONLY;
+            if (mode.equalsIgnoreCase("rw"))
+                m = ParcelFileDescriptor.MODE_READ_WRITE;
 
-	public static Uri getContentUri(File file) {
-	    return Uri.withAppendedPath(FileManagerProvider.CONTENT_URI, file.getAbsolutePath());
-	}
+            Log.d(LOG_TAG, "Trying to open file : " + uri.toString().substring(
+                    CONTENT_URI_STRING.length()));
+            File f = new File(uri.toString().substring(
+                    CONTENT_URI_STRING.length()));
+            try {
+                ParcelFileDescriptor pfd = ParcelFileDescriptor.open(f, m);
+                return pfd;
+            } catch(IOException e) {
+                return null;
+            }
+        } else {
+            throw new RuntimeException("Unsupported uri");
+        }
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentvalues, String s,
+            String[] as) {
+        // not supported
+        return 0;
+    }
+
+    public static Uri getContentUri(File file) {
+        return FileManagerProvider.CONTENT_URI.buildUpon()
+                .encodedPath(file.getAbsolutePath()).build();
+    }
+
+    public static Uri getContentUri(String archive, String entry) {
+        return FileManagerProvider.CONTENT_URI.buildUpon().encodedPath(archive)
+                .encodedFragment(entry).build();
+    }
 }
