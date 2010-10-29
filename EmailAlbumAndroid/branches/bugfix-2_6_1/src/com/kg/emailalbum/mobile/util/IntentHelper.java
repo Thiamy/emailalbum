1ccf3fb3c422db46c6538c89f4b299153feaa924
kevin.gaudin <kevin.gaudin@7dd601f1-8d53-0410-be79-7bf68d38fa1c>
2010-10-29 15:33:28 +0000

    Use custom QuickActions as Action choosers to avoid users from choosing EmailAlbum in the list of possible Activities for sending the final album. This possible choice the most frequent cause of application crash.

--- EmailAlbumAndroid/branches/bugfix-2_6_1/src/com/kg/emailalbum/mobile/util/IntentHelper.java
+++ EmailAlbumAndroid/branches/bugfix-2_6_1/src/com/kg/emailalbum/mobile/util/IntentHelper.java
@@ -1,16 +1,20 @@
 package com.kg.emailalbum.mobile.util;
 
+import static com.kg.emailalbum.mobile.util.Reversed.reversed;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import com.kg.oifilemanager.filemanager.FileManagerProvider;
-import static com.kg.emailalbum.mobile.util.Reversed.reversed;
 
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+import android.view.View;
+
+import com.kg.emailalbum.mobile.creator.EmailAlbumEditor;
+import com.kg.emailalbum.mobile.ui.QuickAction;
+import com.kg.oifilemanager.filemanager.FileManagerProvider;
 
 public class IntentHelper {
     private final static String LOG_TAG = IntentHelper.class.getSimpleName();
@@ -27,10 +31,38 @@ public class IntentHelper {
      * @param body
      *          The message body
      */
+    public static void sendAllPicturesInFolder(View view, File folder, String subject, String body) {
+        Intent sendIntent = prepareSendFolderIntent(folder, subject, body);
+        
+        QuickAction.buildChooser(view, sendIntent, true).show();
+        
+//        Intent chooserIntent = new Intent(activityCtx, ActivityPickerActivity.class);
+//        chooserIntent.putExtra(ActivityPickerActivity.EXTRA_QUERY_INTENT, sendIntent);
+//        activityCtx.startActivity(chooserIntent);
+    }
+
     public static void sendAllPicturesInFolder(Context activityCtx, File folder, String subject, String body) {
+        Intent sendIntent = prepareSendFolderIntent(folder, subject, body);
+        activityCtx.startActivity(sendIntent);
+        
+        
+//        Intent chooserIntent = new Intent(activityCtx, ActivityPickerActivity.class);
+//        chooserIntent.putExtra(ActivityPickerActivity.EXTRA_QUERY_INTENT, sendIntent);
+//        activityCtx.startActivity(chooserIntent);
+    }
+
+    /**
+     * @param folder
+     * @param subject
+     * @param body
+     * @return
+     */
+    private static Intent prepareSendFolderIntent(File folder, String subject, String body) {
         ArrayList<Uri> uris = new ArrayList<Uri>();
         for (File item : reversed(Arrays.asList(folder.listFiles()))) {
-            uris.add(FileManagerProvider.getContentUri(item));
+            if(!item.getName().startsWith(".")) {
+                uris.add(FileManagerProvider.getContentUri(item));
+            }
         }
 
         Intent sendIntent = new Intent(Compatibility.getActionSendMultiple());
@@ -39,7 +71,9 @@ public class IntentHelper {
         sendIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
         Log.d(LOG_TAG, "Sending list of Uris : " + uris.toString());
         sendIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+        sendIntent.putExtra(EmailAlbumEditor.EXTRA_EMAILALBUM_INTENT, true);
         sendIntent.setType("image/*");
-        activityCtx.startActivity(sendIntent);
+        return sendIntent;
     }
+
 }
